6d1d51fd11751aecd27699d223d7af1e2cb14581
Besiiiii <bh58330@ubt-uni.net>
2023-11-19 19:11:46 +0200

    adding stuff

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,14 +1,12 @@
+import './App.css';
 import { Home } from './components/Home'
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
-        
-      <div className="App">
-                < Home />
-            </div>
-   
+        < Header />
+        < Home />
     </div>
   );
 }
---
+++ ClientApp/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return( 
+        <div className="head">
+          <h1>Hello Headeri</h1>
+        </div>
+    )
+}
