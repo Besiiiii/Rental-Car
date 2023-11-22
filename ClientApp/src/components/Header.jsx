9d8b698f6a9cf4d2f3a96dd8a6c89cd9e9531710
Besiiiii <bh58330@ubt-uni.net>
2023-11-22 22:40:12 +0200

    shtimi i buttonave ne header per testim

--- ClientApp/src/components/Header.jsx
+++ ClientApp/src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
     return( 
         <div className="head">
           <h1>Hello Headeri</h1>
+          <div>
+          <h1>Hello</h1>
+          <button ><Link to='/signin'>Log in</Link></button>
+          <button><Link to='/catalog'>Catalog</Link></button>
+        </div>
         </div>
     )
 }
